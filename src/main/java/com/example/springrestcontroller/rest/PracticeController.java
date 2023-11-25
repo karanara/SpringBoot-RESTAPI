6f9c4eae2bd42001786144d3e367b457eb463bdc
karanara <karanara@mail.uc.edu>
2023-11-25 14:44:43 -0500

    rest controller with path variables and exception handling

--- src/main/java/com/example/springrestcontroller/rest/PracticeController.java
+++ src/main/java/com/example/springrestcontroller/rest/PracticeController.java
@@ -3,6 +3,9 @@ package com.example.springrestcontroller.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,8 +33,33 @@ public class PracticeController {
 	//defining end point for students{student}
 	@GetMapping("/student/{studentid}")
 	public Student getStudent(@PathVariable int studentid) {
+		//if student id is greater than size and negative
+		if((studentid > students.size()) || studentid < 0){
+			//thrpowing an exception
+			throw new  StudentNotFoundException("Student id not found -  "+ studentid);
+		}
 		return students.get(studentid);
 		
+	}
+	//Adding a exceptional handler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex){
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(ex.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+	}
+    //exce[ptional handler to catch any kind of exception
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception ex){
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(ex.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
 	}
 
 }
---
+++ src/main/java/com/example/springrestcontroller/rest/StudentErrorResponse.java
@@ -0,0 +1,35 @@
+package com.example.springrestcontroller.rest;
+
+public class StudentErrorResponse {
+
+	public int status;
+	public String message;
+	public long timeStamp;
+	public StudentErrorResponse(int status, String message, long timeStamp) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.timeStamp = timeStamp;
+	}
+	public StudentErrorResponse() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+}
